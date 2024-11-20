4c534e9a4880796d07cd884d4ee858e4113e1156
adharsh-18 <gurudev.adharsh@gmail.com>
2024-11-20 09:51:05 +0530

    poposapso

--- a.py
+++ a.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Welcome Arjun Python<h1>"
+    return redirect(url_for("login"))
 
-if __name__=='__main__':
-    app.run(host="0.0.0.0",port=5000)
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
 
-# print("Hello World")
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+if __name__ == "__main__":
+    app.run("0.0.0.0",5000)
